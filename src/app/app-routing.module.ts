373a2a51d1b4f305912af574b1e658a38ca060a4
GavinFitzpatrick189 <gavin.fitzpatrick@rewardinsight.com>
2020-10-22 12:20:22 +0100

    Adding side menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'inbox',
+    loadChildren: () => import('./inbox/inbox.module').then( m => m.InboxPageModule)
   }
 ];
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,38 @@
 import { Component } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
-export class AppComponent {
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
+export class AppComponent{
+  public selectedIndex = 0;
+  public appPages = [
+    {
+      title: 'Home',
+      url: '/tabs',
+      icon: 'home'
+    },
+    {
+      title: 'Inbox',
+      url: '/inbox',
+      icon: 'mail'
+    }
+  ];
+  constructor() {
     this.initializeApp();
   }
 
   initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
+  }
+
+  ngOnInit() {
+    const path = window.location.pathname.split('/')[1];
+    console.log(path)
+    if (path !== undefined) {
+      this.selectedIndex = this.appPages.findIndex(page => page.title.toLowerCase() === path.toLowerCase());
+    }
   }
 }
